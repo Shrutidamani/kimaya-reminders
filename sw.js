85dbdd7f2f03525783fc3de038ea83c62e42d8b6
Shrutidamani <kabrakk21@gmail.com>
2026-09-10 11:49:36 +0530

    Auto-sync update from PC

--- sw.js
+++ sw.js
@@ -1,22 +1,27 @@
-const CACHE_NAME = 'kimaya-cache-v1';
+const CACHE_NAME = 'kimaya-cache-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
